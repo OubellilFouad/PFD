e8e31d675fa4b6ba1897eb0f091ccbddda64d522
MohamedAouadi <nitroxmoh@gmail.com>
2023-03-17 22:42:45 +0100

    Created first structure

---
+++ Back_end_laravel/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Suppor\Facades\Request;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/test',[TestController::class,'test']);
