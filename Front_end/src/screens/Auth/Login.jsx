3962907662c916e4c9ba1140d1fda948834fcc29
OubellilFouad <oubellilfouad4@gmail.com>
2023-03-17 20:31:33 +0100

    added state to auth

--- Front_end/src/screens/Auth/Login.jsx
+++ Front_end/src/screens/Auth/Login.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Input from './components/Input'
 
 const Login = () => {
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [confPassword,setConfPassword] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://127.0.0.1:8000/api/test').then((response) => console.log(response));
+    console.log('email:' + email);
+    console.log('password:' + password);
+    console.log('confirm password:' + confPassword);
+    setEmail('');
+    setPassword('');
+    setConfPassword('');
+  }
   return (
-    <form action="" className='w-full px-20 flex flex-col items-center gap-10'>
+    <form onSubmit={(e) => handleSubmit(e)} action="" className='w-full px-20 flex flex-col items-center gap-10'>
         <h1 className='text-3xl text-main font-bold'>Welcome back</h1>
         <div className='w-full flex flex-col gap-8'>
-          <Input name={'Email'} type={'email'}/>
-          <Input name={'Password'} type={'password'}/>
-          <Input name={'Confirm Password'} type={'password'}/>
+          <Input name={'Email'} type={'email'} data={email} setData={setEmail} />
+          <Input name={'Password'} type={'password'} data={password} setData={setPassword} />
+          <Input name={'Confirm Password'} type={'password'} data={confPassword} setData={setConfPassword} />
         </div>
-        <Link to={'../../App'} className='w-2/3 py-2 bg-main text-white rounded-lg text-center'>
-          <button type="submit" >SUBMIT</button>
-        </Link>
+        <button className='w-2/3 py-2 bg-main text-white rounded-lg text-center' type="submit" >SUBMIT</button>
         <p className='text-sm text-paleMain'>Are you new here? <Link  to={'../'} className='text-main font-bold'>Signup</Link></p>
     </form>
   )
--- Front_end/src/screens/Auth/components/Input.jsx
+++ Front_end/src/screens/Auth/components/Input.jsx
@@ -1,22 +1,15 @@
 import { info } from 'autoprefixer';
 import React, { useEffect, useRef } from 'react'
 
-const Input = ({name,type}) => {
+const Input = ({name,type,data,setData}) => {
   const input = useRef();  
-  const handleMouseOut = () => {
-    if(input.current.value !== ''){
-        input.current.style.height = '30px';
-    }else{
-        input.current.style.height = '0px';
-    }
-  }
-  const handleClick = () => {
-    input.current.style.height = '30px'
-  }
+  useEffect(() => {
+    input.current.value = '';
+  },[])
   return (
     <div className='flex flex-col'>
         <label htmlFor={name} className='text-paleMain text-base font-medium cursor-pointer'>{name}</label>
-        <input ref={input} onClick={handleClick} onBlur={handleMouseOut} type={type} id={name} className='px-2 pb-2 h-1 border-b-paleMain text-main font-bold border-b-2 bg-transparent outline-none'/>
+        <input ref={input} value={data} onChange={(e) => setData(e.target.value)} type={type} id={name} className='px-2 pb-2 h-8 border-b-paleMain text-main font-bold border-b-2 bg-transparent outline-none'/>
     </div>
   )
 }
