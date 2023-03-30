3a3191d0de169987806f221ab636e6ebd4cc79d5
MohamedAouadi <nitroxmoh@gmail.com>
2023-03-30 03:44:36 +0200

    new

--- Back_end_laravel/app/Models/ChefDep.php
+++ Back_end_laravel/app/Models/ChefDep.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class ChefDep extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
     protected $table = 'chefdep';
     protected $fillable = ['userName', 'email', 'userID', 'dateNaiss','role','depID'];
 }
--- Back_end_laravel/app/Models/Gestionnaire.php
+++ Back_end_laravel/app/Models/Gestionnaire.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Gestionnaire extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
 
     protected $table = 'gestionnaire';
     protected $fillable = [
